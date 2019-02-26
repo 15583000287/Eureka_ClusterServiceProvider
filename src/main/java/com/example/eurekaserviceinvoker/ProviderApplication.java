3e4d7c6a58edbed08b40be0bb169f1c933c0b3a1
wangxu <593464677>
2019-02-26 18:07:52 +0800

    1

--- src/main/java/com/example/eurekaserviceinvoker/ProviderApplication.java
+++ src/main/java/com/example/eurekaserviceinvoker/ProviderApplication.java
@@ -2,7 +2,6 @@ package com.example.eurekaserviceinvoker;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.Scanner;
