fb2c958323063bce26d76791f3635326cc8b0bd7
wangxu <593464677>
2019-02-27 18:14:24 +0800

    1

--- src/main/java/com/example/eurekaserviceinvoker/controller/FistController.java
+++ src/main/java/com/example/eurekaserviceinvoker/controller/FistController.java
@@ -1,12 +1,9 @@
 package com.example.eurekaserviceinvoker.controller;
 
 import com.example.eurekaserviceinvoker.entity.User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-
 
 @RestController
 public class FistController {
@@ -19,4 +16,22 @@ public class FistController {
         user.setMessage(request.getRequestURL().toString());
         return user;
     }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Ribbon !";
+    }
+
+
+    /**
+     * 测试Feign编码：
+     * @requestBody 可以将请求体中的JSON字符串绑定到相应的bean上，当然，也可以将其分别绑定到对应的字符串上。
+     *  注意： JSON字符串中的key必须对应user中的属性名
+     */
+    @RequestMapping(value = "user/create",method = RequestMethod.POST)
+    public String createUser(@RequestBody User user){
+        System.out.println(user.getUsername() + "-" + user.getPassword());
+        return "Success, Person Id: " + user.getId();
+    }
+
 }
